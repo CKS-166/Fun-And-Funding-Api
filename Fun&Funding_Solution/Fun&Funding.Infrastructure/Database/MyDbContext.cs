25beca4b6d669c33eabde261be4edcbb311e3787
Lâm Minh Đăng <2603lamminhdang@gmail.com>
2024-09-09 10:19:15 +0700

    new

---
+++ Fun&Funding_Solution/Fun&Funding.Infrastructure/Database/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Fun_Funding.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fun_Funding.Infrastructure.Database
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext()
+        {
+            
+        }
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<TestUser> Users { get; set; }
+    }
+}
