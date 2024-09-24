878fb0bb79677d4a2940e4bd99271b2236a59701
baocd2003 <BaoCao494@gmail.com>
2024-09-24 17:08:07 +0700

    create funding project

--- Fun&Funding.Api/Controllers/FundingProjectController.cs
+++ Fun&Funding.Api/Controllers/FundingProjectController.cs
@@ -3,9 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using Fun_Funding.Application.IStorageService;
 using Fun_Funding.Application.IService;
 using Fun_Funding.Application.ViewModel.FundingProjectDTO;
+using Fun_Funding.Application.ViewModel.FundingFileDTO;
 namespace Fun_Funding.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/funding-projects")]
     [ApiController]
     public class FundingProjectController : ControllerBase
     {
@@ -19,30 +20,23 @@ namespace Fun_Funding.Api.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
-        {
-            var response = await _storageService.UploadFiles(files);
-            return Ok(response);
-        }
-
-        [HttpPost("blob")]
-        public async Task<IActionResult> UploadBlobFiles(List<IFormFile> files)
-        {
-            var response = await _storageService.UploadBlobFiles(files);
-            return Ok(response);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetUploadFiles()
-        {
-            var response = await _storageService.GetUploadedItems();
-            return Ok(response);
-        }
-
-        [HttpPost("project-add")]
-        public async Task<IActionResult> CreateProject([FromBody] FundingProjectAddRequest req)
+        public async Task<IActionResult> CreateProject([FromForm] FundingProjectAddRequest req)
         {
             var response = await _fundingProjectService.CreateFundingProject(req);
+            return Ok(response);
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetProjectById([FromQuery] Guid id)
+        {
+            var response = await _fundingProjectService.GetProjectById(id);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProject([FromForm] FundingProjectUpdateRequest req)
+        {
+            var response = await _fundingProjectService.UpdateFundingProject(req);
             return Ok(response);
         }
     }
