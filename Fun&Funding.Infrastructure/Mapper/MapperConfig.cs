5f90456d5904832e22f06565d7873479acf43c58
Lâm Minh Đăng <2603lamminhdang@gmail.com>
2024-09-25 14:22:13 +0700

    update new get all donation

--- Fun&Funding.Infrastructure/Mapper/MapperConfig.cs
+++ Fun&Funding.Infrastructure/Mapper/MapperConfig.cs
@@ -3,9 +3,11 @@ using Fun_Funding.Application.ViewModel.BankAccountDTO;
 using Fun_Funding.Application.ViewModel.CategoryDTO;
 using Fun_Funding.Application.ViewModel.FundingFileDTO;
 using Fun_Funding.Application.ViewModel.FundingProjectDTO;
+using Fun_Funding.Application.ViewModel.PackageBackerDTO;
 using Fun_Funding.Application.ViewModel.PackageDTO;
 using Fun_Funding.Application.ViewModel.RewardItemDTO;
 using Fun_Funding.Domain.Entity;
+using Fun_Funding.Domain.Enum;
 
 namespace Fun_Funding.Infrastructure.Mapper
 {
@@ -17,24 +19,24 @@ namespace Fun_Funding.Infrastructure.Mapper
         {
             MappingFundingProject();
         }
-        
+
         public void MappingFundingProject()
         {
             CreateMap<FundingFileRequest, FundingFile>().ReverseMap();
             CreateMap<ItemAddRequest, RewardItem>().ReverseMap();
             CreateMap<PackageAddRequest, Package>()
-                .ForMember(des => des.RewardItems , src => src.MapFrom(x => x.RewardItems)).ReverseMap();
+                .ForMember(des => des.RewardItems, src => src.MapFrom(x => x.RewardItems)).ReverseMap();
             CreateMap<BankAccountRequest, BankAccount>().ReverseMap();
             CreateMap<PackageUpdateRequest, Package>().ReverseMap();
             CreateMap<ItemUpdateRequest, RewardItem>().ReverseMap();
             CreateMap<FundingFileResponse, FundingFile>().ReverseMap();
             CreateMap<FundingProjectAddRequest, FundingProject>()
                 .ForMember(des => des.BankAccount, src => src.MapFrom(x => x.BankAccount))
-                .ForMember(des => des.Packages , src => src.MapFrom(x => x.Packages)).ReverseMap();
+                .ForMember(des => des.Packages, src => src.MapFrom(x => x.Packages)).ReverseMap();
             CreateMap<FundingProjectResponse, FundingProject>()
                 .ForMember(des => des.BankAccount, src => src.MapFrom(x => x.BankAccount))
-                .ForMember(des => des.Packages, src => src.MapFrom(x => x.Packages)).ReverseMap(); 
-            
+                .ForMember(des => des.Packages, src => src.MapFrom(x => x.Packages)).ReverseMap();
+
             CreateMap<FundingProjectUpdateRequest, FundingProject>()
                 .ForMember(des => des.BankAccount, src => src.MapFrom(x => x.BankAccount))
                 .ForMember(des => des.Packages, src => src.MapFrom(x => x.Packages)).ReverseMap();
@@ -42,6 +44,7 @@ namespace Fun_Funding.Infrastructure.Mapper
             CreateMap<Category, CategoryResponse>().ReverseMap();
             CreateMap<CategoryRequest, Category>().ReverseMap();
         }
+        
     }
 
 
