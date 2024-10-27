152d665ee11ff4ecf4ef23cb93a87c02381564f3
diemyolo <quydiem2015@gmail.com>
2024-10-27 12:21:07 +0700

    minor update add cart authorization

--- Fun&Funding.Api/Controllers/CartController.cs
+++ Fun&Funding.Api/Controllers/CartController.cs
@@ -2,6 +2,8 @@
 using Fun_Funding.Application.IService;
 using Fun_Funding.Application.Services.EntityServices;
 using Fun_Funding.Application.ViewModel;
+using Fun_Funding.Domain.Constrain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,24 +20,28 @@ namespace Fun_Funding.Api.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = Role.Backer)]
         public async Task<IActionResult> GetUserCartInfo()
         {
             var response = await _cartService.GetUserCartInfo();
             return Ok(response);
         }
         [HttpPatch("add/{marketplaceProjectId}")]
+        [Authorize(Roles = Role.Backer)]
         public async Task<IActionResult> AddGameToUserCart([FromRoute]Guid marketplaceProjectId)
         {
             var response = await _cartService.AddGameToUserCart(marketplaceProjectId);
             return Ok(response);
         }
         [HttpPatch("clear")]
+        [Authorize(Roles = Role.Backer)]
         public async Task<IActionResult> ClearCart()
         {
             var response = await _cartService.ClearCart();
             return Ok(response);
         }
         [HttpPatch("delete/{marketplaceProjectId}")]
+        [Authorize(Roles = Role.Backer)]
         public async Task<IActionResult> DeleteCartItem([FromRoute] Guid marketplaceProjectId)
         {
             var response = await _cartService.DeleteCartItem(marketplaceProjectId);
