a06e670c3c149f9f404adfff4f47be96ece246a7
DaoHuongThao <daothao148@gmail.com>
2024-11-01 21:46:59 +0700

    update-delete mp

--- Fun&Funding.Application/Interfaces/IEntityService/IMarketplaceService.cs
+++ Fun&Funding.Application/Interfaces/IEntityService/IMarketplaceService.cs
@@ -1,6 +1,7 @@
 ﻿using Fun_Funding.Application.ViewModel;
 using Fun_Funding.Application.ViewModel.MarketplaceProjectDTO;
 using Fun_Funding.Domain.Entity;
+using Fun_Funding.Domain.Enum;
 
 namespace Fun_Funding.Application.IService
 {
@@ -10,6 +11,9 @@ namespace Fun_Funding.Application.IService
         Task<ResultDTO<MarketplaceProjectInfoResponse>> CreateMarketplaceProject(MarketplaceProjectAddRequest request);
         Task<ResultDTO<MarketplaceProjectInfoResponse>> GetMarketplaceProjectById(Guid id);
         Task DeleteMarketplaceProject(Guid id);
-        Task<ResultDTO<MarketplaceProjectInfoResponse>> UpdateMarketplaceProject(Guid id, MarketplaceProjectUpdateRequest request);
+        Task<ResultDTO<MarketplaceProjectInfoResponse>>
+            UpdateMarketplaceProject(Guid id, MarketplaceProjectUpdateRequest request, bool? isDeleted = null);
+        Task<ResultDTO<MarketplaceProjectInfoResponse>>
+            UpdateMarketplaceProjectStatus(Guid id, ProjectStatus status);
     }
 }
