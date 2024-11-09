b95d3f79369d4d986ad9aea1a1ff7a25ed7443e5
DaoHuongThao <daothao148@gmail.com>
2024-11-09 16:13:27 +0700

    fix mp bank account

--- Fun&Funding.Domain/Entity/FundingProject.cs
+++ Fun&Funding.Domain/Entity/FundingProject.cs
@@ -1,11 +1,6 @@
 ﻿using Fun_Funding.Domain.Enum;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fun_Funding.Domain.Entity
 {
@@ -21,11 +16,11 @@ namespace Fun_Funding.Domain.Entity
         public decimal Target { get; set; }
         [Range(0, (double)decimal.MaxValue)]
         [Column(TypeName = "decimal(18, 2)")]
-        public decimal Balance { get; set; }    
+        public decimal Balance { get; set; }
         public ProjectStatus Status { get; set; }
 
         public Wallet? Wallet { get; set; }
-        public MarketplaceProject? MarketingProject { get; set; }
+        public MarketplaceProject? MarketplaceProject { get; set; }
         public User User { get; set; }
         public Guid UserId { get; set; }
 
