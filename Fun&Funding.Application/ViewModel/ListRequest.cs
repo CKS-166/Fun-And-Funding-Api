a996cc3c9b24821e226c31ced63b6959830d4cc5
DaoHuongThao <daothao148@gmail.com>
2024-12-15 10:02:54 +0700

    fix add category

--- Fun&Funding.Application/ViewModel/ListRequest.cs
+++ Fun&Funding.Application/ViewModel/ListRequest.cs
@@ -5,7 +5,7 @@
         public string? OrderBy { get; set; }
         public string? SearchValue { get; set; }
         public int? PageIndex { get; set; } = 1;
-        public int? PageSize { get; set; } = 999;
+        public int? PageSize { get; set; } = 10;
         public bool? IsAscending { get; set; } = false;
         public object? From { get; set; }
         public object? To { get; set; }
